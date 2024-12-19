55e983b5f939edc8f73053facb00f38ffa62a3d3
obmbane <obmbaneaws@gmail.com>
2024-12-19 23:47:55 +0200

    added hotels.csv and created the available and book methods in the hotel class

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import pandas as pd
 
 class Hotel:
 
-    def __init__(self, id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def list(self):
+
         pass
 
     def book(self):
-        pass
+        df['available'].loc[df['id'] == self.hotel_id] = 'no'
+        df.to_csv('hotels.csv')
 
     def available(self):
-        pass
+
+        status = df['available'][df['id'] == self.hotel_id].squeeze()
+        print(status)
+
+        if status == 'yes':
+            test = 13 + car 
+            print(test)
+            return True
+
+        else:
+            return False
 
 class Receipt:
 
@@ -22,14 +34,15 @@ class Receipt:
     def generate(self):
         pass
 
-df = pd.read_csv('hotels.csv')
+df = pd.read_csv('hotels.csv', dtype={'id':str})
 
 print(df)
 
-hotel_id = input("Please Enter Hotel ID: ")
-hotel = Hotel(hotel_id)
+hotel_ID = input("Please Enter Hotel ID: ")
+hotel = Hotel(hotel_ID)
+car = 31
 
-if hotel.available() == 'yes':
+if hotel.available():
     hotel.book()
     customer_name = input("Please Enter Your Name and Surname: ")
     customer_receipt = Receipt(hotel, customer_name)
