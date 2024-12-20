804a535a608813fab9fe374a16e88fb25907dd0b
obmbane <obmbaneaws@gmail.com>
2024-12-20 16:02:00 +0200

    Added SecureCreditCard Class which inherits the CeditCard Class

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ class Hotel:
 
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
-        self.name = df['name'].loc[df['id'] == hotel_id].squeeze()
+        self.name = df_hotels['name'].loc[df_hotels['id'] == hotel_id].squeeze()
 
     def list(self):
 
         pass
 
     def book(self):
-        df['available'].loc[df['id'] == self.hotel_id] = 'no'
-        df.to_csv('hotels.csv', index=False)
+        df_hotels['available'].loc[df_hotels['id'] == self.hotel_id] = 'no'
+        df_hotels.to_csv('hotels.csv', index=False)
 
     def available(self):
 
-        status = df['available'][df['id'] == self.hotel_id].squeeze()
+        status = df_hotels['available'][df_hotels['id'] == self.hotel_id].squeeze()
 
         if status == 'yes':
             return True
@@ -69,29 +69,51 @@ class CreditCard:
         else:
             return False
 
+class SecureCreditCard(CreditCard):
+    def authenticate(self,user_password):
+
+        system_password = df_card_security['password'].loc[df_card_security['number'] == self.number].squeeze()
+
+        if system_password == user_password:
+            return True
+        
+        else:
+            return False
 
 
-df = pd.read_csv('hotels.csv', dtype={'id':str})
+df_hotels = pd.read_csv('hotels.csv', dtype={'id':str})
 df_card = pd.read_csv('cards.csv', dtype=str).to_dict(orient="records")
-print(df_card)
+df_card_security = pd.read_csv('card_security.csv', dtype=str)
+print(df_hotels)
 
 hotel_ID = input("Please Enter Hotel ID: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
+
     cc_holder = input("Enter Name on Credit Card: ").upper()
     cc_num = input("Enter Credit Card Number: ")
     cc_exp = input("Enter Credit Card Expiry Date: ")
     cc_cvc = input("Enter Credit Card CVC Number: ")
 
-    credit_card = CreditCard(cc_num)
+    credit_card = SecureCreditCard(cc_num)
+
     if credit_card.validate(holder=cc_holder,expiration=cc_exp,cvc=cc_cvc):
-        print("Payment Validated!")
-        hotel.book()
-        customerName = input("Please Enter Your Name and Surname: ")
-        customer_receipt = Receipt(hotel_name=hotel.name, customer_name=customerName)
-        receipt = customer_receipt.generate()
-        print(receipt)
+
+        print("Credit Card Validated!")
+        user_password = input("Please Enter Your Password: ")
+
+        if credit_card.authenticate(user_password):
+
+            hotel.book()
+            customerName = input("Please Enter Your Name and Surname: ")
+            customer_receipt = Receipt(hotel_name=hotel.name, customer_name=customerName)
+            receipt = customer_receipt.generate()
+            print(receipt)
+
+        else:
+                print('Credit Card Authentication Failed')
+
     else:
         print("Issue with payment, please make sure credit card details are correct")
 
